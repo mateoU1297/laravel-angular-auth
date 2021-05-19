99b19ada029a5aa9d9f8a0aebcb96d6b089f6019
mateoU1297 <castro.mateo@correounivalle.edu.co>
2021-05-19 00:58:34 -0500

    rutas de angular

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: 'login',
+  component: LoginComponent
+},
+{
+  path: 'sign-up',
+  component: SignupComponent
+},
+{
+  path: 'profile',
+  component: ProfileComponent
+},
+{
+  path: 'request-reset',
+  component: RequestResetComponent
+},
+{
+  path: 'response-reset',
+  component: ResponseResetComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
